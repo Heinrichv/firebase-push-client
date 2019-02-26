957b99954c1f9d8909eaafdcce00b2f7cd5ed6c4
heinrichv <heinrichv@osiristrading.com>
2019-02-26 10:47:51 +0200

    test

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const compression = require('compression');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT;
+app.use(compression());
+app.use(cors());
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+
+app.use(express.static('dist', {
+    dotfiles: "deny",
+    etag: true,
+    extensions: ["js", "css", "json", "png", "html"],
+    index: false,
+    maxAge: "1d",
+    lastModified: true,
+    redirect: false,
+    setHeaders: function (res, path, stat) {
+        res.set("x-timestamp", Date.now());
+    }
+}));
+
+app.listen(port);
---
+++ webpack.config.js
@@ -0,0 +1,79 @@
+﻿const webpack = require('webpack'); 
+const path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+const extractSass = new ExtractTextPlugin({
+    filename: "[name].[contenthash].css",
+    disable: process.env.NODE_ENV === "development"
+});
+
+module.exports = {
+    entry: {
+
+        //TypeScript Examples
+        // 'scripts/typescript/helpers/BrowserStorage': "./src/scripts/typescript/helpers/BrowserStorage.ts",
+
+        //Javascript Examples
+        //'scripts/merlin/pariplay/wintingo' : "./src/scripts/merlin/bundleConfig/pariplay.js",
+        //'scripts/merlin/helpers/helper' : "./src/scripts/merlin/bundleConfig/helpers.js",
+        
+        //SASS Examples
+        //'css/common/bootstrap': "./src/content/sass/bootstrap/bootstrap.scss"
+
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].min.js'
+    },
+    externals: {
+        "jquery" : /^(jquery|\$)$/i
+    },
+    module: {
+        rules: [
+            {
+                test: /\.txt$/,
+                exclude: /node_modules/,
+                use: 'raw-loader'
+            },
+            {
+                test: /\.tsx?$/,
+                exclude: /node_modules/,
+                loader: "awesome-typescript-loader"
+            },
+            {
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: "style-loader"
+                    },
+                    {
+                        loader: "css-loader"
+                    },
+                    {
+                        loader: "sass-loader"
+                    }
+                ]
+            },
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: "style-loader"
+                    },
+                    {
+                        loader: "css-loader"
+                    }
+                ]
+            }
+        ]
+    },
+    resolve: {
+        extensions: [".js", ".json", ".jsx", ".css", ".ts", ".scss"]
+    },
+    plugins: [
+        extractSass,
+        new webpack.optimize.UglifyJsPlugin()
+    ]
+};
