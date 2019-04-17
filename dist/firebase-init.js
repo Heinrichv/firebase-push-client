1e74edf6be3d7dfe86d5be2c2d661861956c791c
Heinrich Venter <heini141@outlook.com>
2019-04-17 17:33:02 +0200

    Update firebase-init.js

--- dist/firebase-init.js
+++ dist/firebase-init.js
@@ -1,197 +1,112 @@
-var config = {
-  authDomain: "propify-c0f15.firebaseapp.com",
-  databaseURL: "https://propify-c0f15.firebaseio.com",
-  projectId: "propify-c0f15",
-  messagingSenderId: "508221299021"
-};
-
-var vapid = 'BMHsTfD_KmLFAOOzm9mDl9w9LOhtF8mw7yAZma6bM1MJmbyb1Oh7R-dunh6pATepEM7VqpdMe7IviwXWRflVOF4';
-
-firebase.initializeApp(config);
+firebase.initializeApp(window.static.firebase);
 
 const messaging = firebase.messaging();
+messaging.usePublicVapidKey(window.static.data.vapid);
 
-// Add the public key generated from the console here.
-messaging.usePublicVapidKey(vapid);
-
-// IDs of divs that display Instance ID token UI or request permission UI.
-const tokenDivId = 'token_div';
-const permissionDivId = 'permission_div';
-
-// [START refresh_token]
-// Callback fired if Instance ID token is updated.
 messaging.onTokenRefresh(function () {
   messaging.getToken().then(function (refreshedToken) {
     console.log('Token refreshed.');
-    // Indicate that the new Instance ID token has not yet been sent to the
-    // app server.
     setTokenSentToServer(false);
-    // Send Instance ID token to app server.
     sendTokenToServer(refreshedToken);
-    // [START_EXCLUDE]
-    // Display new Instance ID token and clear UI of all previous messages.
-    resetUI();
-    // [END_EXCLUDE]
   }).catch(function (err) {
     console.log('Unable to retrieve refreshed token ', err);
     showToken('Unable to retrieve refreshed token ', err);
   });
 });
-// [END refresh_token]
 
-// [START receive_message]
-
-// Handle incoming messages. Called when:
-// - a message is received while the app has focus
-// - the user clicks on an app notification created by a service worker
-//   `messaging.setBackgroundMessageHandler` handler.
 messaging.onMessage(function (payload) {
-  console.log('Message received. ', payload);
+  // on message show push
+  console.log('Message Received. ', payload);
+
+  if (!payload.data) {
+    payload.data = {};
+  }
 
   new Notification(payload.notification.title, {
     body: payload.notification.body,
-    image: payload.notification.icon,
-    icon: payload.notification.icon
-  })
-
-  appendMessage(payload);
+    icon: payload.notification.icon,
+    image: payload.data.hasOwnProperty('gcm.notification.image') ? payload.data['gcm.notification.image'] : undefined
+  });
 });
 
-// [END receive_message]
-
-function resetUI() {
-  clearMessages();
-  showToken('loading...');
-  // [START get_token]
-  // Get Instance ID token. Initially this makes a network call, once retrieved
-  // subsequent calls to getToken will return from cache.
-  messaging.getToken().then(function (currentToken) {
-    if (currentToken) {
-      sendTokenToServer(currentToken);
-      updateUIForPushEnabled(currentToken);
-    } else {
-      // Show permission request.
-      console.log('No Instance ID token available. Request permission to generate one.');
-      // Show permission UI.
-      updateUIForPushPermissionRequired();
-      setTokenSentToServer(false);
-    }
-  }).catch(function (err) {
-    console.log('An error occurred while retrieving token. ', err);
-    showToken('Error retrieving Instance ID token. ', err);
-    setTokenSentToServer(false);
-  });
-  // [END get_token]
-}
-
-
-function showToken(currentToken) {
-  // Show token in console and UI.
-  var tokenElement = document.querySelector('#token');
-  tokenElement.textContent = currentToken;
-}
-
-// Send the Instance ID token your application server, so that it can:
-// - send messages back to this app
-// - subscribe/unsubscribe the token from topics
 function sendTokenToServer(currentToken) {
+  // send to server
   if (!isTokenSentToServer()) {
-    console.log('Sending token to server...');
-    // TODO(developer): Send the current token to your server.
+    const request = {
+      "accountId": "00000000-0000-0000-0000-000000000000",
+      "registrationToken": currentToken,
+      "culture": window.static.data.culture,
+      "countryCode": window.static.data.countryCode,
+      "currency": window.static.data.currency,
+      "masterToken": window.static.data.masterToken,
+      "bannerTag": window.static.data.bannerTag,
+      "brandCode": window.static.data.brandCode,
+      "siteCode": window.static.data.siteCode
+    };
+
+    console.log('Sending token to server...', currentToken);
+    subscribe(request);
 
     setTokenSentToServer(true);
   } else {
-    console.log('Token already sent to server so won\'t send it again ' +
-      'unless it changes');
+    console.log('Token already exists on server, logging token activity', currentToken);
+    logTokenActivity(currentToken);
   }
+}
 
+function subscribe(request) {
+  // send token to server for storing
+  const xmlHttp = new XMLHttpRequest();
+  xmlHttp.open("POST", `${window.static.data.pushApi}/api/v1/register/subscribe`);
+  xmlHttp.setRequestHeader('Content-Type', 'application/json');
+  xmlHttp.send(JSON.stringify(request));
+}
+
+function logTokenActivity(token) {
+  // log token activity to server and keep last active date updated for token.
+  const xmlHttp = new XMLHttpRequest();
+  xmlHttp.open("PUT", `${window.static.data.pushApi}/api/v1/register/subscriber/${token}`);
+  xmlHttp.setRequestHeader('Content-Type', 'application/json');
+  xmlHttp.send(null);
 }
 
 function isTokenSentToServer() {
-  return window.localStorage.getItem('sentToServer') === '1';
+  // check if token has already been sent to the server
+  return window.localStorage.getItem('sts') === '1';
 }
 
 function setTokenSentToServer(sent) {
-  window.localStorage.setItem('sentToServer', sent ? '1' : '0');
-}
-
-function showHideDiv(divId, show) {
-  const div = document.querySelector('#' + divId);
-  if (show) {
-    div.style = 'display: visible';
-  } else {
-    div.style = 'display: none';
-  }
+  // set that token has already been sent to the server
+  window.localStorage.setItem('sts', sent ? '1' : '0');
 }
 
 function requestPermission() {
-  console.log('Requesting permission...');
-  // [START request_permission]
+  // initialize accept notifications popup
   messaging.requestPermission().then(function () {
-    console.log('Notification permission granted.');
-    // TODO(developer): Retrieve an Instance ID token for use with FCM.
-    // [START_EXCLUDE]
-    // In many cases once an app has been granted notification permission, it
-    // should update its UI reflecting this.
-    resetUI();
-    // [END_EXCLUDE]
+    messaging.getToken().then(function (refreshedToken) {
+      sendTokenToServer(refreshedToken);
+    });
   }).catch(function (err) {
     console.log('Unable to get permission to notify.', err);
   });
-  // [END request_permission]
 }
 
-function deleteToken() {
-  // Delete Instance ID token.
-  // [START delete_token]
-  messaging.getToken().then(function (currentToken) {
-    messaging.deleteToken(currentToken).then(function () {
-      console.log('Token deleted.');
-      setTokenSentToServer(false);
-      // [START_EXCLUDE]
-      // Once token is deleted update UI.
-      resetUI();
-      // [END_EXCLUDE]
-    }).catch(function (err) {
-      console.log('Unable to delete token. ', err);
-    });
-    // [END delete_token]
-  }).catch(function (err) {
-    console.log('Error retrieving Instance ID token. ', err);
-    showToken('Error retrieving Instance ID token. ', err);
-  });
-
-}
-
-// Add a message to the messages element.
-function appendMessage(payload) {
-  const messagesElement = document.querySelector('#messages');
-  const dataHeaderELement = document.createElement('h5');
-  const dataElement = document.createElement('pre');
-  dataElement.style = 'overflow-x:hidden;';
-  dataHeaderELement.textContent = 'Received message:';
-  dataElement.textContent = JSON.stringify(payload, null, 2);
-  messagesElement.appendChild(dataHeaderELement);
-  messagesElement.appendChild(dataElement);
-}
-
-// Clear the messages element of all children.
-function clearMessages() {
-  const messagesElement = document.querySelector('#messages');
-  while (messagesElement.hasChildNodes()) {
-    messagesElement.removeChild(messagesElement.lastChild);
+function getCookie(name) {
+  const cookieObj = this.getAllCookies();
+  if (cookieObj.hasOwnProperty(name)) {
+    return cookieObj[name];
   }
+  return null;
 }
 
-function updateUIForPushEnabled(currentToken) {
-  showHideDiv(tokenDivId, true);
-  showHideDiv(permissionDivId, false);
-  showToken(currentToken);
+function getAllCookies() {
+  const cookieArr = document.cookie.split(';');
+  const cookieObj = {};
+  cookieArr.forEach((value) => {
+    const split = value.trim().split('=');
+    cookieObj[split[0]] = split[1];
+  });
+  return cookieObj;
 }
 
-function updateUIForPushPermissionRequired() {
-  showHideDiv(tokenDivId, false);
-  showHideDiv(permissionDivId, true);
-}
-
-resetUI();
+requestPermission();
